ffa5bda4276349a15bd929e4877bdf67ecfe2276
vivek-khandhar-07 <vivekkhamdhar07@gmail.com>
2026-01-03 11:31:33 +0530

    Add files via upload

---
+++ two.py
@@ -0,0 +1,18 @@
+x = 20
+print (x)
+y = 20.5
+print (y)
+z = 1j
+print(z)
+a = True 
+b = False
+print(a)
+print(b)
+c = None
+print(c)    
+e = ["a","b","c","d"]
+print(e)
+f = ("a","b","c","d")
+print(f)
+g = ({"a","b","c","d"})
+print(g) 
