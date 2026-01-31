63a8810a845c07cb21789eba99b970148a54722c
vivek-khandhar-07 <vivekkhamdhar07@gmail.com>
2026-01-31 10:34:40 +0530

    Add files via upload

---
+++ io_1.py
@@ -0,0 +1,13 @@
+#input-output statement statement
+#output statement
+print("hello world!!")
+print("hello,vivek")
+
+#input statement
+name=input("enter your name :")
+print("hello",name)
+park=input("enter your name :")
+print("vivek",park)
+
+#sys.argv statement
+print(sys.argv)
