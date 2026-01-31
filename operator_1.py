29b13e2792f2a3965f85bd6c8ba27c0d4b8d8fea
vivek-khandhar-07 <vivekkhamdhar07@gmail.com>
2026-01-31 10:51:48 +0530

    Add files via upload

---
+++ operator_1.py
@@ -0,0 +1,30 @@
+#1.Assignment operator
+x=10
+print(x)
+
+#2.unary minus operator
+a=5
+print(-a)
+
+#3.relational operator
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+#4.logical operator 
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#5.boolean operator
+ispass=True
+print(ispass)
+
+#6bitwise operators
+v=5
+k=3
+print(a & b)
+print(a | b)
