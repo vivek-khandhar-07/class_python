1c9b716281bc3c14394cc81261809797fa4e81c4
vivek-khandhar-07 <vivekkhamdhar07@gmail.com>
2026-02-07 10:39:47 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,17 @@
+marks=int(input("enter your marks :"))
+if marks>=90:
+    print("you will get grade A")
+elif marks>=70:
+    print("you will get grade B")
+elif marks>=60:
+    print("you will get grade C")
+elif marks>=50:
+    print("you will get grade D")
+else:
+    print("better luck next time")
+    
+age=int(input("enter your age"))
+if age>=18:
+    print("yes, you are eligible for vote")
+else:
+    print("sorry you are not eligible for vote")
