9ed848cb5b17a696453db0c0579a06c92165f0f7
vivek-khandhar-07 <vivekkhamdhar07@gmail.com>
2026-04-04 11:31:30 +0530

    Add files via upload

---
+++ opening-files.py
@@ -0,0 +1,15 @@
+'''# read a file
+
+f1 = open("one.txt", "r")
+data = f1.read()
+print(data)
+
+# write a file 
+
+f2 = open("two.txt","w")
+vk = f2.write()
+print(vk)'''
+
+
+f = open("myfile.txt","r")
+
